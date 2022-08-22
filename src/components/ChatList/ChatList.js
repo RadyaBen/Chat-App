28a003492e9ca5dae189530c8122c6a5a25f6801
RadyaBen <lizoguben.radya@gmail.com>
2022-08-22 11:03:33 +0300

    Add a stylized avatar image to the chat list component

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -1,9 +1,20 @@
+import avatarImage from '../../assets/images/anonymous-avatar.png';
+
 import './ChatList.scss';
 
 const ChatList = () => {
 	return (
 		<div className='chatlist'>
-
+			<div className='chatlist__box'>
+				<div className='chatlist__avatar'>
+					<img
+						className='chatlist__image'
+						src={avatarImage}
+						alt='Anonymous Avatar'
+						title='Anonymous Avatar'
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
