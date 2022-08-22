42c7a0606edf4c483b91ac1317d194fbd7694027
RadyaBen <lizoguben.radya@gmail.com>
2022-08-22 13:01:10 +0300

    Add dummy chat users' data

---
+++ src/data/chatUsersData.js
@@ -0,0 +1,44 @@
+const chatUsersData = [
+	{
+		image: "http://emilcarlsson.se/assets/louislitt.png",
+		id: 1,
+		name: "Louis Litt",
+		createdDate: "Aug 16, 2022",
+		message: "I'm doing ok. Just working on building some applications to bulk up my resume, so I can get a better job",
+		isOnline: true,
+	},
+	{
+		image: "http://emilcarlsson.se/assets/harveyspecter.png",
+		id: 2,
+		name: "Harvey Specter",
+		createdDate: "Jul 18, 2022",
+		message: "No problem. Can I help you with anything else?",
+		isOnline: false,
+	},
+	{
+		image: "http://emilcarlsson.se/assets/rachelzane.png",
+		id: 3,
+		name: "Rachel Zane",
+		createdDate: "Jun 20, 2022",
+		message: "Yes, it was! It was nice meeting you as well. What was your favorite session?",
+		isOnline: false,
+	},
+	{
+		image: "http://emilcarlsson.se/assets/donnapaulsen.png",
+		id: 4,
+		name: "Donna Paulsen",
+		createdDate: "Jun 16, 2022",
+		message: "Thank you for always being so positive!",
+		isOnline: true,
+	},
+	{
+		image: "http://emilcarlsson.se/assets/jessicapearson.png",
+		id: 5,
+		name: "Jessica Pearson",
+		createdDate: "May 24, 2022",
+		message: "Well, you shoud take some time to relax then. What do you find relaxing to do?",
+		isOnline: false,
+	},
+];
+
+export { chatUsersData };
