5cc0a86932ddedc90f17a3ecfa4db6eda7c100d7
RadyaBen <lizoguben.radya@gmail.com>
2022-08-30 18:27:06 +0300

    Implement the functionality of adding a message

--- src/components/ChatView/ChatView.js
+++ src/components/ChatView/ChatView.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { ChatItem } from '../ui/ChatItem';
 import { Avatar } from '../ui/Avatar';
@@ -6,23 +7,72 @@ import { chatItemsData } from '../../data';
 
 const ChatView = ({ selectedChatId }) => {
 
-	const [chats, setChats] = useState(chatItemsData);
+	const [chatItems, setChatItems] = useState(
+		JSON.parse(localStorage.getItem('conversation')) || chatItemsData
+	);
+	const [chatMessage, setChatMessage] = useState('');
 
-	const chat = chats.find(chat => chat.id === selectedChatId); // By default, the selectedChatId value is set to 1
-	const conversation = chat.conversation.map(item => item);
+	const selectedChat = chatItems.find(chat => chat.id === selectedChatId); // By default, the selectedChatId value is set to 1
+
+	useEffect(() => {
+		localStorage.setItem('conversation', JSON.stringify(chatItems));
+	}, [chatItems]);
+
+	const handleInputChange = (e) => {
+		setChatMessage(e.target.value);
+	};
+
+	const handleKeyDown = (e) => {
+		if (e.keyCode === 13) {
+			addMessageToChat();
+		}
+	};
+
+	const getCurrentDateTime = () => {
+		const dateObj = new Date();
+		const options = { hour: 'numeric', hour12: true, minute: 'numeric' };
+
+		return dateObj.toLocaleDateString('en', options);
+	};
+
+	const addMessageToChat = () => {
+
+		if (!chatMessage) {
+			return
+		}
+
+		const newMessageItem = {
+			key: uuidv4(),
+			image: 'http://emilcarlsson.se/assets/mikeross.png',
+			type: '',
+			createdDate: getCurrentDateTime(),
+			message: chatMessage,
+		};
+
+		setChatItems((prevChatItem) => {
+			return prevChatItem.map((chatItem) => {
+				return chatItem.id === selectedChatId
+					// Add the message to the current conversation
+					? { ...chatItem, conversation: [...chatItem.conversation, newMessageItem] }
+					: chatItem
+			});
+		});
+
+		setChatMessage('');
+	};
 
 	return (
 		<div className='chat-view'>
 			<div className='chat-view__header'>
 				<div className='profile-wrapper'>
 					<div className='profile-wrapper__current-user'>
-						{chat && (
+						{selectedChat && (
 							<>
 								<Avatar
-									image={chat.profileImage}
+									image={selectedChat.profileImage}
 									isOnline='active'
 								/>
-								<p>{chat.profileName}</p>
+								<p>{selectedChat.profileName}</p>
 							</>
 						)}
 					</div>
@@ -30,16 +80,18 @@ const ChatView = ({ selectedChatId }) => {
 			</div>
 			<div className='chat-view__content'>
 				<div className='chat-view__items'>
-					{conversation && conversation.map((chatItem) => (
-						<ChatItem
-							key={chatItem.key}
-							image={chatItem.image}
-							user={chatItem.type ? chatItem.type : 'me'}
-							createdDate={chatItem.createdDate}
-							time={chatItem.time}
-							message={chatItem.message}
-						/>
-					))}
+					{selectedChat.conversation &&
+						selectedChat.conversation.map((chatItem) => (
+							<ChatItem
+								key={chatItem.key}
+								image={chatItem.image}
+								user={chatItem.type ? chatItem.type : 'me'}
+								createdDate={chatItem.createdDate}
+								time={chatItem.time}
+								message={chatItem.message}
+							/>
+						))
+					}
 				</div>
 			</div>
 			<div className='chat-view__footer'>
@@ -47,11 +99,14 @@ const ChatView = ({ selectedChatId }) => {
 					<input
 						type='text'
 						id='chat-message'
+						value={chatMessage}
 						placeholder='Type your message'
+						onChange={handleInputChange}
+						onKeyDown={handleKeyDown}
 					/>
-					<label htmlFor='chat-message'>
+					<button htmlFor='chat-message' onClick={addMessageToChat}>
 						<span className='fa fa-paper-plane'></span>
-					</label>
+					</button>
 				</div>
 			</div>
 		</div>
