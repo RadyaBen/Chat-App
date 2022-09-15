ad4a8d05937aea312638059802712fb427aefc78
RadyaBen <lizoguben.radya@gmail.com>
2022-09-15 16:50:07 +0300

    Set an auto-reply for user text messages

--- src/components/ChatView/ChatView.js
+++ src/components/ChatView/ChatView.js
@@ -1,22 +1,75 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
 import { ChatItem } from '../ui/ChatItem';
 import { Avatar } from '../ui/Avatar';
 import { chatItemsData } from '../../data';
+import messageNotification from '../../assets/sounds/message-notification.mp3';
 
 const ChatView = ({ selectedChatId }) => {
-
 	const [chatItems, setChatItems] = useState(
 		JSON.parse(localStorage.getItem('conversation')) || chatItemsData
 	);
 	const [chatMessage, setChatMessage] = useState('');
+	const [randomJokeMessage, setRandomJokeMessage] = useState(null);
+	const [isBotMessage, setIsBotMessage] = useState(false);
+	const [isClicked, setIsClicked] = useState(false);
+	let timeoutRef = useRef(null);
+	let messagesEndRef = useRef(null);
 
 	const selectedChat = chatItems.find(chat => chat.id === selectedChatId); // By default, the selectedChatId value is set to 1
+	const audio = new Audio(messageNotification);
+
+	useEffect(() => {
+		const controller = new AbortController();
+
+		const axiosRequest = async () => {
+			try {
+				const { data } = await axios.get('https://api.chucknorris.io/jokes/random/');
+				setRandomJokeMessage(data.value);
+			} catch (error) {
+				if (axios.isAxiosError(error)) {
+					console.log('Axios Error with Message: ' + error.message);
+				} else {
+					console.log(error);
+				}
+			}
+		};
+
+		axiosRequest();
+
+		return () => {
+			controller.abort();
+		};
+	}, [isBotMessage]);
 
 	useEffect(() => {
 		localStorage.setItem('conversation', JSON.stringify(chatItems));
+
+		if (messagesEndRef.current) {
+			scrollToBottom();
+		}
 	}, [chatItems]);
+
+	useEffect(() => {
+		if (isClicked) {
+
+			if (timeoutRef.current) {
+				// Clear the running timer and start a new one each time the user clicks
+				clearTimeout(timeoutRef.current);
+			}
+
+			timeoutRef.current = setTimeout(() => {
+				addMessageToChat(true);
+				audio.play();
+			}, 5000);
+
+			setIsBotMessage(false);
+			setIsClicked(false);
+		}
+		// eslint-disable-next-line
+	}, [isClicked]);
 
 	const handleInputChange = (e) => {
 		setChatMessage(e.target.value);
@@ -24,8 +77,23 @@ const ChatView = ({ selectedChatId }) => {
 
 	const handleKeyDown = (e) => {
 		if (e.keyCode === 13) {
+
+			if (isDisabled()) {
+				return
+			}
+			
+			setIsClicked(true);
+			setIsBotMessage(true);
 			addMessageToChat();
 		}
+	};
+
+	const scrollToBottom = () => {
+		messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+	};
+
+	const isDisabled = () => {
+		return chatMessage.trim().length === 0;
 	};
 
 	const getCurrentDateTime = () => {
@@ -35,30 +103,27 @@ const ChatView = ({ selectedChatId }) => {
 		return dateObj.toLocaleDateString('en', options);
 	};
 
-	const addMessageToChat = () => {
+	const addMessageToChat = (isRandomAutoresponse) => {
+		if (chatMessage || isRandomAutoresponse) {
+			const newMessageItem = {
+				key: uuidv4(),
+				image: isBotMessage ? selectedChat.profileImage : 'http://emilcarlsson.se/assets/mikeross.png',
+				type: isBotMessage ? 'other' : '',
+				createdDate: getCurrentDateTime(),
+				message: isBotMessage ? randomJokeMessage : chatMessage,
+			};
 
-		if (!chatMessage) {
-			return
-		}
-
-		const newMessageItem = {
-			key: uuidv4(),
-			image: 'http://emilcarlsson.se/assets/mikeross.png',
-			type: '',
-			createdDate: getCurrentDateTime(),
-			message: chatMessage,
-		};
-
-		setChatItems((prevChatItem) => {
-			return prevChatItem.map((chatItem) => {
-				return chatItem.id === selectedChatId
-					// Add the message to the current conversation
-					? { ...chatItem, conversation: [...chatItem.conversation, newMessageItem] }
-					: chatItem
+			setChatItems((prevChatItem) => {
+				return prevChatItem.map((chatItem) => {
+					return chatItem.id === selectedChatId
+						// Add the message to the current conversation
+						? { ...chatItem, conversation: [...chatItem.conversation, newMessageItem] }
+						: chatItem
+				});
 			});
-		});
 
-		setChatMessage('');
+			setChatMessage('');
+		}
 	};
 
 	return (
@@ -80,18 +145,25 @@ const ChatView = ({ selectedChatId }) => {
 			</div>
 			<div className='chat-view__content'>
 				<div className='chat-view__items'>
-					{selectedChat.conversation &&
-						selectedChat.conversation.map((chatItem) => (
-							<ChatItem
-								key={chatItem.key}
-								image={chatItem.image}
-								user={chatItem.type ? chatItem.type : 'me'}
-								createdDate={chatItem.createdDate}
-								time={chatItem.time}
-								message={chatItem.message}
-							/>
-						))
-					}
+					{selectedChat.conversation.length === 0 ? (
+						<p>No messages here yet...</p>
+					) : (
+						<>
+							{selectedChat.conversation &&
+								selectedChat.conversation.map((chatItem) => (
+									<ChatItem
+										key={chatItem.key}
+										image={chatItem.image}
+										user={chatItem.type ? chatItem.type : 'me'}
+										createdDate={chatItem.createdDate}
+										time={chatItem.time}
+										message={chatItem.message}
+									/>
+								))
+							}
+						</>
+					)}
+					<div ref={messagesEndRef} />
 				</div>
 			</div>
 			<div className='chat-view__footer'>
@@ -104,7 +176,14 @@ const ChatView = ({ selectedChatId }) => {
 						onChange={handleInputChange}
 						onKeyDown={handleKeyDown}
 					/>
-					<button htmlFor='chat-message' onClick={addMessageToChat}>
+					<button
+						disabled={isDisabled()}
+						htmlFor='chat-message'
+						onClick={() => {
+							addMessageToChat();
+							setIsClicked(true);
+							setIsBotMessage(true);
+						}}>
 						<span className='fa fa-paper-plane'></span>
 					</button>
 				</div>
