5684189a37b8eb378357b982bb75bccfbd509e57
RadyaBen <lizoguben.radya@gmail.com>
2022-10-03 23:40:51 +0300

    Connect the chat list to the Redux Toolkit

--- src/components/ChatBody/ChatBody.js
+++ src/components/ChatBody/ChatBody.js
@@ -1,21 +1,13 @@
-import { useState } from 'react';
-
 import { ChatList } from '../ChatList';
 import { ChatView } from '../ChatView';
 
 import './ChatBody.scss';
 
 const ChatBody = () => {
-	const [activeChatId, setActiveChatId] = useState(1);
-
-	const handleActiveChat = (activeId) => {
-		setActiveChatId(activeId)
-	};
-
 	return (
 		<div className='chatbody'>
-			<ChatList onSelectedChatId={handleActiveChat} />
-			<ChatView selectedChatId={activeChatId} />
+			<ChatList />
+			<ChatView />
 		</div>
 	);
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import chatReducer from '../features/chat/chatSlice';
+
 const store = configureStore({
-	reducer: {},
+	reducer: {
+		chat: chatReducer,
+	},
 });
 
 export { store };
